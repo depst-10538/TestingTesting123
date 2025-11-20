03207ccd1cc667e2b2c859e260a88310cf4d42e2
depst-10538 <tur12015@temple.edu>
2025-11-20 14:39:02 -0500

    Added tests to ensure proper function for adding items

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,14 +16,23 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        val firstSize = collection.size()
+        collection.addItem(Item("newItem"))
+        val secondSize = collection.size()
 
+        assert(secondSize == firstSize+1) {"Items not added"}
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        collection.addItem(Item("newItem"))
+        val firstSize = collection.size()
+        collection.addItem(Item("newItem"))
+        val secondSize = collection.size()
 
+        assert(firstSize == secondSize) {"Duplicates not rejected"}
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
